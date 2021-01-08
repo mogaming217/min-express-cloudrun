30549f22159cebb5cf77473bbf6985f8c373c00f
mogaming <seiya.orz@gmail.com>
2021-01-08 13:40:11 +0900

    ADD: helmet

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ dotenv.config({ path: `${process.cwd()}/env/.env.${appEnv}` })
 // これ以下にコードを書く
 
 import express from 'express'
+import helmet from 'helmet'
 import { env } from './env'
 
 const PORT = parseInt(process.env.PORT || '4000', 10)
 const app = express()
+
+// middlewares
+app.use(helmet())
+
+ // routes
 
 app.get('/', (_, res) => {
   res.json({ hey: 'yo', nodeEnv: process.env.NODE_ENV ?? 'none', appEnv: env.appEnv ?? 'none' })
